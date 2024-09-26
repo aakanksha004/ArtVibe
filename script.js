0bbac1229fc83bb92cd17c62c171d5bbfe61dc24
aakanksha004 <aakankshatiwari2004@gmail.com>
2024-09-26 22:22:52 +0530

    2nd changes

--- script.js
+++ script.js
@@ -28,6 +28,8 @@ h4all.forEach(function(elem){
 
 
 
+
+
 gsap.to("#nav",{
      backgroundColor:"#000",
      height: "100px",
@@ -70,20 +72,24 @@ gsap.from("#about-us img,#about-us-in", {
   });
 
   
-gsap.from(".card", {
-   scale:0.8,
-    opacity: 0,
-    duration: 1,
-    stagger:0.1,
-    scrollTrigger: {
-      trigger: ".card",
-      scroller: "body",
-      // markers:true,
-      start: "top 70%",
-      end: "top 65%",
-      scrub: 1,
-    },
-  });
+gsap.set(".card", { scale: 0.8, opacity: 0 }); // Set initial state for all cards
+
+gsap.to(".card", {
+  scale: 1,
+  opacity: 1,
+  duration: 1,
+  stagger: 0.2,
+  scrollTrigger: {
+    trigger: "#cards-container", // Change trigger to the container
+    scroller: "body",
+    markers: false,
+    start: "top 70%",
+    end: "top 65%",
+    scrub: 1,
+  }
+});
+
+  
   gsap.from("#colon1", {
     y: -70,
     x: -70,
